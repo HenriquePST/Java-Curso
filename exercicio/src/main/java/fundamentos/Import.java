538e17acedc050d788791fbff6f1fbc2c15993ea
Joao <joaohenrique2@hotmail.com>
2024-12-21 00:29:11 -0300

    Utilização  de uso da classe Date e importação em Java

---
+++ exercicio/src/main/java/fundamentos/Import.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+
+// Utilização  de uso da classe Date e importação em Java
+//
+//- Demonstração de como importar e usar a classe Date.
+//- Exibição de data e hora atual usando a classe Date.
+
+import java.util.Date;
+
+public class Import {
+    public static void main(String[] args) {
+
+
+        String s = "Bom dia";
+        System.out.println(s);
+
+        Date d = new Date();
+        System.out.println(d);
+
+
+
+    }
+}
