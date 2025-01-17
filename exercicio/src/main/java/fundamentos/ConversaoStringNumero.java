8cf9c9d1bf0efedbf8769436aebdc2b75beb04ab
Joao <joaohenrique2@hotmail.com>
2025-01-17 19:34:06 -0300

    realizei uma  conversão de String para número com cálculo de média e multiplicação

--- exercicio/src/main/java/fundamentos/ConversaoStringNumero.java
+++ exercicio/src/main/java/fundamentos/ConversaoStringNumero.java
@@ -1,4 +1,21 @@
 package fundamentos;
 
+import javax.swing.*;
+import java.math.BigDecimal;
+
 public class ConversaoStringNumero {
+    public static void main(String[] args) {
+        String valor1 = JOptionPane.showInputDialog (
+                "Digite o primeiro numero");
+        String valor2 = JOptionPane.showInputDialog(
+                "Digite o segundo numero");
+        System.out.println(valor1 + valor2);
+        double numero1 = Double.parseDouble(valor1);
+        double numero2 = Double.parseDouble(valor2);
+
+        double resultado = numero1 * numero2;
+        System.out.println("soma é " +resultado);
+        System.out.println("media é " +resultado/2);
+
+    }
 }
