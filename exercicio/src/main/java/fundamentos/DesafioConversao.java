f046be49d857ebac7e523a8e6a1ddce57681ce15
Joao <joaohenrique2@hotmail.com>
2025-01-18 18:12:26 -0300

    desafioconversa

---
+++ exercicio/src/main/java/fundamentos/DesafioConversao.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+
+//Crie uma classe chamada DesafioConversao. Nessa classe, use o Scanner para capturar três strings
+// com o método nextLine().
+// Essas strings representarão os últimos três salários de um funcionário.
+//
+//Implemente a lógica para:
+//
+//Converter as strings para números (considerando que os valores podem usar vírgula ou ponto como
+// separador decimal).
+//Somar os três salários.
+//Calcular a média dos últimos três salários.
+//Como você faria para implementar essa conversão e realizar os cálculos?"
+
+public class DesafioConversao {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+    }
+}
