a183038016a3aa38be7dd465c053aecebeb840fb
Joao <joaohenrique2@hotmail.com>
2025-01-24 19:18:41 -0300

    add alguns comentario tenario

---
+++ exercicio/src/main/java/controle/IfElseIf.java
@@ -0,0 +1,18 @@
+package controle;
+
+import java.util.Scanner;
+
+public class IfElseIf {
+    public static void main(String[] args) {
+        Scanner Entrada = new Scanner(System.in);
+
+        System.out.println("digite um numero: ");
+        double numero = Entrada.nextDouble();
+
+        if (numero > 10 || nova < 0) {
+            System.out.println("numero invalido");
+        } else {
+            System.out.println("numero valido");
+        }
+    }
+}
--- exercicio/src/main/java/fundamentos/operadores/Tenario.java
+++ exercicio/src/main/java/fundamentos/operadores/Tenario.java
@@ -3,8 +3,16 @@ package fundamentos.operadores;
 public class Tenario {
     public static void main(String[] args) {
         double media = 7.7;
+        // condicao ? expressao_1 : expressao_2
         String resulta = media >= 7.0 ? " aprovado" : "reprovado" ;
         System.out.println( "o aluno está " + resulta );
+
+//        if(media >= 7.0){
+//            System.out.println("Aprovado");
+//        } else {
+//            System.out.println("Reprovado");
+//        }
+//        System.out.println(resulta);
 
 
         double nota = 9.9;
