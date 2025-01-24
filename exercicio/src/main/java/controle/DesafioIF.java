a9ba2f2b9486215b53e77a79e415dbb21bc4511a
Joao <joaohenrique2@hotmail.com>
2025-01-23 23:49:20 -0300

    desafio if e a exceção do ; ponto e virgula

---
+++ exercicio/src/main/java/controle/DesafioIF.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class DesafioIF {
+    public static void main(String[] args) {
+
+         double nota = 1.2;
+
+         // nãoo usar ; em estruturas de controle ( tem um exceção)
+         if (nota >= 9.0) {
+             System.out.println("fera");
+             System.out.println("quadro de hora");
+         }
+    }
+}
