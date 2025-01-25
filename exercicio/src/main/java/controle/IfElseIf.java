1ea94fb8a88ee739b243ea051f44b3fb08fb3254
Joao <joaohenrique2@hotmail.com>
2025-01-25 19:08:00 -0300

    estrutura IfElseIf

--- exercicio/src/main/java/controle/IfElseIf.java
+++ exercicio/src/main/java/controle/IfElseIf.java
@@ -4,15 +4,26 @@ import java.util.Scanner;
 
 public class IfElseIf {
     public static void main(String[] args) {
-        Scanner Entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
 
-        System.out.println("digite um numero: ");
-        double numero = Entrada.nextDouble();
+        System.out.println("digite a nota: ");
+        double nota = entrada.nextDouble();
 
-        if (numero > 10 || nova < 0) {
+        if (nota > 10 || nota < 0) {
             System.out.println("numero invalido");
+        } else if (nota >= 8.1) {
+            System.out.println("Conceito A");
+        } else if (nota >= 6.1) {
+            System.out.println("conceito B");
+        } else if (nota >= 4.1) {
+            System.out.println("conceito C");
+        } else if (nota >= 2.1) {
+            System.out.println("conceito D");
         } else {
-            System.out.println("numero valido");
+            System.out.println("conceito E");
         }
+        System.out.println("fim");
+        entrada.close();
+
     }
 }
